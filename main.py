27310364fa14d2cdc889a82ba35437bb80425479
WilliamSpanfelner <102249249+WilliamSpanfelner@users.noreply.github.com>
2022-06-14 17:59:11 +0300

    Test for successful secret login
    and display success.html or denied.html depending.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 from forms import LoginForm
 # See https://wtforms.readthedocs.io/en/3.0.x/crash_course/
 """ Command to check verion of Flask (from CLI) and WTForms respectively: 
@@ -19,7 +19,11 @@ def home():
 @app.route("/login", methods=["GET", "POST"])
 def login():
     login_form = LoginForm(request.form)
-    login_form.validate_on_submit()
+    if login_form.validate_on_submit():
+        if login_form.email.data == 'admin@email.com' and login_form.password.data == '12345678':
+            return render_template('success.html')
+        else:
+            return render_template('denied.html')
     return render_template('login.html', form=login_form)
 
 
